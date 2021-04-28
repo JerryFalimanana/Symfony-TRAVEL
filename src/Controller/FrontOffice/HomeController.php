8027b3a89506661330e0c4fd65a6daed3ac908a1
Jerry Falimanana <jfr@esokia-webagency.com>
2021-04-28 14:39:28 +0300

    twig initiation

---
+++ src/Controller/FrontOffice/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\FrontOffice;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $nombre = [1,2,3,4,5,6,7,8,9];
+
+        return $this->render('front_office/home/index.html.twig', [
+            'chiffre' => $nombre,
+        ]);
+    }
+
+    /**
+     * Fonction avec route parametré
+     * 
+     * @Route("/hello/{prenom}", name="vody")
+     *
+     * @param [type] $prenom
+     * @return Response
+     */
+    public function hello($prenom): Response
+    {
+        return new Response("Bonjour " . $prenom);
+    }
+}
