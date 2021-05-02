c0d9aee17965965ed349d04ebec7b8e495ed2824
Jerry Falimanana <jfr@esokia-webagency.com>
2021-05-02 23:26:31 +0300

    creation 10 fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Add;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 10; $i++) {
+            $add = new Add;
+    
+            $add->setTitle("Titre de l'annonce n°$i")
+                ->setSlug("titre-de-l-annonce-n-$i")
+                ->setCoverImage("httm://placehold.it/1000x300")
+                ->setIntroduction("Voici donc une introduction poir commencer")
+                ->setContent("<p>Une contenue simple mais si riche que vous ne le pensez pas")
+                ->setPrice(mt_rand(40, 200))
+                ->setRooms(mt_rand(1, 5));
+    
+            $manager->persist($add);
+        }
 
         $manager->flush();
     }
