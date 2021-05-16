6ad9e8977a4f4919ce0c23ed88c6f2c8d76967c2
Jerry Falimanana <jfr@esokia-webagency.com>
2021-05-16 14:01:55 +0300

    #0404 show one ad with repository

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,18 +6,36 @@ use App\Repository\AddRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(AddRepository $repo): Response
+    public function index(AddRepository $repo, SessionInterface $session): Response
     {
         // $repo = $this->getDoctrine()->getRepository(Add::class);
         $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @param [type] $slug
+     * @param AddRepository $repo
+     * @return Response
+     */
+    public function show($slug, AddRepository $repo): Response
+    {
+        $ad = $repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad,
         ]);
     }
 }
