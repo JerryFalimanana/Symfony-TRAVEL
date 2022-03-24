e29e7fb6fb84911d48f1167878a985388958d333
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-03-24 17:22:07 +0300

    admin form connection

--- src/Controller/AdminAccountController.php
+++ src/Controller/AdminAccountController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAccountController extends AbstractController
 {
     /**
      * @Route("/admin/login", name="admin_account_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
         return $this->render('back_office/account/login.html.twig', [
-            'controller_name' => 'AdminAccountController',
+            'hasError' => $error !== null,
+            'userName' => $username
         ]);
     }
 }
