7a7f3f739da35463f3e7b9a9c9cacce56459ac87
JerryFalimanana <ratsimbajaona1006@gmail.com>
2022-04-04 22:22:12 +0300

    use EntityType for form booking edit

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminBookingController extends AbstractController
 {
@@ -16,6 +18,23 @@ class AdminBookingController extends AbstractController
     {
         return $this->render('back_office/booking/index.html.twig', [
             'bookings' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     *  Permet de modifier une réservation
+     *
+     * @Route("/admin/bookings/{id}/edit", name="admin_booking_edit")
+     * 
+     * @return Response
+     */
+    public function edit(Booking $booking): Response
+    {
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        return $this->render('back_office/booking/edit.html.twig', [
+            'form' => $form->createView(),
+            'booking' => $booking,
         ]);
     }
 }
