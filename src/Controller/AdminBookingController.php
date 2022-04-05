d165408511791df8e8d17a8e66310cec85d39325
JerryFalimanana <ratsimbajaona1006@gmail.com>
2022-04-05 22:08:09 +0300

    save booking update

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,9 +30,22 @@ class AdminBookingController extends AbstractController
      * 
      * @return Response
      */
-    public function edit(Booking $booking): Response
+    public function edit(Booking $booking, Request $request, EntityManagerInterface $manager): Response
     {
         $form = $this->createForm(AdminBookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $manager->persist($booking); comme la réservation est déjà reconu par le manager
+            $manager->flush(); // on utilise tout simplement cette ligne
+            $this->addFlash(
+                'success',
+                "La réservation n°{$booking->getId()} est bien modifiée"
+            );
+
+            return $this->redirectToRoute("admin_booking_index");
+        }
 
         return $this->render('back_office/booking/edit.html.twig', [
             'form' => $form->createView(),
