52650d56229ec4bbef9f021b880543165da98390
JerryFalimanana <ratsimbajaona1006@gmail.com>
2022-04-06 22:37:00 +0300

    create delete function for booking in Admin

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -54,4 +54,24 @@ class AdminBookingController extends AbstractController
             'booking' => $booking,
         ]);
     }
+
+    /**
+     *  Permet de supprimer une réservation
+     *
+     * @Route("/admin/bookings/{id}/delete", name="admin_booking_delete")
+     * 
+     * @return Response
+     */
+    public function delete(Booking $booking, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($booking);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "La réservation a bien été supprimée"
+        );
+
+        return $this->redirectToRoute("admin_booking_index");
+    }
 }
