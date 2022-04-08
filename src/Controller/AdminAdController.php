f89b3a69f387352789e034729c98f0d86bbd3b43
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-04-08 12:22:47 +0300

    paging integration

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -14,12 +14,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ads", name="admin_ads_index")
+     * @Route("/admin/ads/{page}", name="admin_ads_index")
      */
-    public function index(AddRepository $repo): Response
+    public function index(AddRepository $repo, $page = 1): Response
     {
+        $limit = 10;
+        $start = $page * $limit - $limit;
+
         return $this->render('back_office/ad/index.html.twig', [
-            'ads' => $repo->findAll(),
+            'ads' => $repo->findBy([], [], $limit, $start),
         ]);
     }
 
