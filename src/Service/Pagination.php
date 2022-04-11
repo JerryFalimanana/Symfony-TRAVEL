3134e05c48d91cc9d44fda0910a50803d5d162b4
jerryfalimanana <ratsimbajaona1006@gmail.com>
2022-04-11 15:45:59 +0300

    use RequestStack to find the current route

--- src/Service/Pagination.php
+++ src/Service/Pagination.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Environment;
 
 class Pagination
 {
@@ -10,10 +12,23 @@ class Pagination
     private $limit = 10;
     private $entityClass;
     private $manager;
+    private $twig;
+    private $route;
 
-    public function __construct(EntityManagerInterface $manager)
+    public function __construct(EntityManagerInterface $manager, Environment $twig, RequestStack $request)
     {
+        $this->route = $request->getCurrentRequest()->attributes->get('_route');
+        
         $this->manager = $manager;
+        $this->twig = $twig;
+    }
+
+    public function display() {
+        $this->twig->display('back_office/partials/_pagination.html.twig', [
+            'page' => $this->currentPage,
+            'pages' => $this->getPages(),
+            'route' => $this->route
+        ]);
     }
 
     public function getPages() {
@@ -62,5 +77,25 @@ class Pagination
 
     public function getEntityClass() {
         return $this->entityClass;
+    }
+
+    /**
+     * Get the value of route
+     */ 
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set the value of route
+     *
+     * @return  self
+     */ 
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
     }
 }
